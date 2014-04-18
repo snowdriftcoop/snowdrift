7276f3ca763bd779fd39a29136e286bc53e6b323
jazzyeagle <jazzyeagle79@gmail.com>
2014-04-18 16:01:17 -0600

    Fixes to tickets 903 & 976

--- Widgets/ProjectPledges.hs
+++ Widgets/ProjectPledges.hs
@@ -6,6 +6,31 @@ import Import
 import Model.Project
 import Model.Currency
 
+projectPledgeSummary :: UserId -> Widget
+projectPledgeSummary user_id = do
+    project_summary :: [ProjectSummary] <- handlerToWidget $ runDB $ do
+        projects_pledges <- fmap (map (second return)) $ select $ from $ \ (project `InnerJoin` pledge) -> do
+            on_ $ project ^. ProjectId ==. pledge ^. PledgeProject
+            where_ $ pledge ^. PledgeUser ==. val user_id
+            return (project, pledge)
+
+        mapM (uncurry summarizeProject) projects_pledges
+
+    toWidget [hamlet|
+        $if null project_summary
+            not supporting any projects
+        $else
+            <table .table>
+                <tr>
+                    <td>
+                        <a href="@{UserPledgesR user_id}">
+                            $if (length project_summary) == 1
+                                <p>Patron to 1 project
+                            $else
+                                <p>Patron to (length project_summary) projects
+     |]
+
+
 projectPledges :: UserId -> Widget
 projectPledges user_id = do
     project_summaries :: [ProjectSummary] <- handlerToWidget $ runDB $ do
@@ -25,7 +50,7 @@ projectPledges user_id = do
             not supporting any projects
         $else
             <p>
-                note: for testing purposes only, no real money is changing hands yet
+                Note: For testing purposes only.  No real money is changing hands yet
             <table .table>
                 $forall summary <- project_summaries
                     <tr>
@@ -33,6 +58,10 @@ projectPledges user_id = do
                             <a href="@{ProjectR (summaryProjectHandle summary)}">
                                 #{summaryName summary}
                         <td>#{show (cost summary)}/share
-                        <td>#{show (shares summary)}&nbsp;shares
+                        <td>
+                            $if (shares summary) == 1
+                                #{show (shares summary)} share
+                            $else
+                                #{show (shares summary)} shares
                         <td>#{show (total summary)}
-    |]
+     |]
