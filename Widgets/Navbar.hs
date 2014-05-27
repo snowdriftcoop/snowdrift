9524e1e0f1b657b8eac2f399b59d1647f7bd98c9
wolftune <wolftune@gmail.com>
2014-05-27 15:51:00 -0700

    spacing clean-up and overall refactoring of Navbar.hs

--- Widgets/Navbar.hs
+++ Widgets/Navbar.hs
@@ -18,9 +18,10 @@ navbar = do
 
     role_values <- case maybe_user of
         Nothing -> return []
-        Just (Entity user_id _) -> handlerToWidget $ runDB $ select $ from $ \ project_user_role -> do
-            where_ $ project_user_role ^. ProjectUserRoleUser ==. val user_id
-            return $ project_user_role ^. ProjectUserRoleRole
+        Just (Entity user_id _) ->
+            handlerToWidget $ runDB $ select $ from $ \ project_user_role -> do
+                where_ $ project_user_role ^. ProjectUserRoleUser ==. val user_id
+                return $ project_user_role ^. ProjectUserRoleRole
 
     let is_committee_member = False -- TODO
 
@@ -29,32 +30,37 @@ navbar = do
         sb_link route name = do
             authorized <- handlerToWidget $ isAuthorized route False
             case authorized of
-             Authorized -> [whamlet|
+                Authorized -> [whamlet|
                     $if maybe_route == Just route
                         #{string name}
                     $else
                         <a href=@{route}>
                             #{string name}
                     <br>
-                |]
+                    |]
 
-             _ -> return ()
+                _ -> return ()
 
     money_info <- case maybe_user of
         Nothing -> return Nothing
         Just (Entity user_id user) -> do
             (pledges, balance) <- handlerToWidget $ runDB $ do
-                pledges :: [(Entity Project, Entity Pledge)] <- select $ from $ \ (project `InnerJoin` pledge) -> do
-                    on_ $ pledge ^. PledgeProject ==. project ^. ProjectId
-                    where_ $ pledge ^. PledgeUser ==. val user_id
-                    return (project, pledge)
+                pledges :: [(Entity Project, Entity Pledge)] <- select $ from $
+                    \ (project `InnerJoin` pledge) -> do
+                        on_ $ pledge ^. PledgeProject ==. project ^. ProjectId
+                        where_ $ pledge ^. PledgeUser ==. val user_id
+                        return (project, pledge)
 
                 Just account <- get (userAccount user)
                 return (pledges, accountBalance account)
-            let pledged = sum $ map (\ (project, pledge) -> ((projectShareValue (entityVal project) $*) . fromIntegral . pledgeFundedShares . entityVal) pledge) pledges
+
+            let pledged = sum $ map (\ (project, pledge) ->
+                    ((projectShareValue (entityVal project) $*) . fromIntegral . pledgeFundedShares . entityVal) pledge) pledges
+
             return $ Just (balance, pledged)
 
 
+    -- TODO: make stuff below generalize to project affiliates instead of snowdrift only
 
     (messages, applications, edits, comments) <- case maybe_user of
         Nothing -> return ([], [], 0, 0)
@@ -63,27 +69,28 @@ navbar = do
             projects <- select $ from $ \ project -> return project
 
             messages :: [Entity Message] <- select $ from $ \ message -> do
-                            where_ $ let readSnowdriftMessages =
-                                             ( message ^. MessageCreatedTs >=. val (userReadMessages user)
-                                               &&. isNothing (message ^. MessageTo))
+                where_ $
+                    let
+                        readSnowdriftMessages =
+                            ( message ^. MessageCreatedTs >=. val (userReadMessages user)
+                                &&. isNothing (message ^. MessageTo))
 
-                                         readUserMessages =
-                                            ( message ^. MessageCreatedTs >=. val (userReadMessages user)
-                                              &&. message ^. MessageTo ==. val (Just user_id))
-
-                                      in if snowdrift_member
-                                          then readSnowdriftMessages ||. readUserMessages
-                                          else readUserMessages
-
-                            return message
+                        readUserMessages =
+                            ( message ^. MessageCreatedTs >=. val (userReadMessages user)
+                                &&. message ^. MessageTo ==. val (Just user_id))
+                     in if snowdrift_member
+                            then readSnowdriftMessages ||. readUserMessages
+                            else readUserMessages
+                return message
 
 
             applications :: [Entity VolunteerApplication] <- if is_committee_member
-                             then select $ from $ \ application -> do
-                                where_ ( application ^. VolunteerApplicationCreatedTs >=. val (userReadApplications user) )
-                                return application
-                            else return []
-            {-
+                then select $ from $ \ application -> do
+                    where_ $ application ^. VolunteerApplicationCreatedTs >=. val (userReadApplications user)
+                    return application
+                else return []
+
+            {- *Aaron asks: what is this commented-out stuff here??*
             edits :: [Entity WikiEdit] <- select $ from $ \ wiki_edit -> do
                     where_ ( wiki_edit ^. WikiEditTs >=. val (userReadEdits user) ) -- &&. wiki_edit ^. WikiEditUser !=. val user_id )
                     return wiki_edit
@@ -92,6 +99,7 @@ navbar = do
                     where_ ( comment ^. CommentCreatedTs >=. val (userReadComments user) ) -- &&. comment ^. CommentUser !=. val user_id )
                     return comment
             -}
+
             let unval (Value a) = a
                 foldCounts (c1, c2) (c1', c2') =
                     (c1 + unval (L.head c1'), c2 + unval (L.head c2'))
