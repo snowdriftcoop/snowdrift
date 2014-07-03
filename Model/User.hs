2fed1291f0391978a952d281003f331dc1b40ca9
wolftune <wolftune@gmail.com>
2014-07-03 00:01:59 -0700

    removed the reason part of the message for eligibility for now we can deal with it later as we figure that out, it's in the DB anyway

--- Model/User.hs
+++ Model/User.hs
@@ -119,10 +119,12 @@ eligEstablishUser establisher_id user_id reason = do
   where
     message_text :: Markdown
     message_text = Markdown $ T.unlines
-        [ "Because" <> reason <> ","
-        , "you are now eligible to become an *established* user."
+        -- "Because" <> reason <> ","
+        -- That reason could be added when we find a good way to fit it in,
+        -- but for now it is just stored in the database
+        [ "You are now eligible to become an *established* user."
         , ""
-        , "After you [accept the honor pledge](/honor-pledge), you can comment and take other actions on the site without moderation. "
+        , "After you [accept the honor pledge](/honor-pledge), you can comment and take other actions on the site without moderation."
         ]
 
 -- | Get a User's Roles in a Project.
