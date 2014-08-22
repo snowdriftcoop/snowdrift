b352b6acb183ac7ddcd84251664f77749da462d9
wolftune <wolftune@gmail.com>
2014-08-22 16:44:13 -0700

    added back lower preview alert

--- Widgets/Preview.hs
+++ Widgets/Preview.hs
@@ -13,6 +13,7 @@ previewWidget form action widget =
 
         <div .alert .alert-danger>
             This is a preview; your changes have <em>not</em> been saved!
+
         <form #edit-preview method="POST">
             <button type="submit" name="mode" value="preview">preview
             <button .preview-action-button type="submit" name="mode" value="post">#{action}
