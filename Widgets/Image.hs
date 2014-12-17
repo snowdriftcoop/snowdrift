935d5cc057f32f7ec7f2620f7f79a597b8e951a9
Jason Harrer <jazzyeagle79@gmail.com>
2014-12-17 08:32:16 -0700

    Image Widget 2/3rds complete, remainder to be ticketed for someone else to complete, per Aaron.

--- Widgets/Image.hs
+++ Widgets/Image.hs
@@ -1,14 +1,10 @@
 module Widgets.Image (imageSelectField) where
 
 import Import
-import Yesod.Form.Functions (parseHelper)
 
 imageSelectField :: [Entity Image] -> Field Handler Text   -- [Entity Image]
 imageSelectField images = Field
     { fieldParse = parseHelper $ Right
--- \value _ -> case value of
---        ("":_) -> return $ Right Nothing
---        (x:_) -> return $ either (Left . SomeMessage) (Right . Just) x
     , fieldView = \idAttr nameAttr attrs result _ -> $(widgetFile "image_selector")
     , fieldEnctype = UrlEncoded
     }
