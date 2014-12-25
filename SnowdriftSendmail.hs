0f38822c37ec901d3f534c0ef4cfc1e43da3e25e
Nikita Karetnikov <nikita@karetnikov.org>
2014-12-26 00:21:34 +0300

    Decode before returning the result.

--- SnowdriftSendmail.hs
+++ SnowdriftSendmail.hs
@@ -2,13 +2,18 @@
 
 module Main where
 
-import qualified Data.ByteString.Char8 as C
-import           System.Environment          (getArgs)
-import           System.IO                   (stdin)
-import           Data.Monoid                 ((<>))
+import qualified Codec.MIME.QuotedPrintable as QuotedPrintable
+import           Data.ByteString.Char8      (ByteString)
+import qualified Data.ByteString.Char8      as C
+import           System.Environment         (getArgs)
+import           System.IO                  (stdin)
+import           Data.Monoid                ((<>))
 
 main :: IO ()
 main = getArgs >>= \case
-    []     -> C.hGetContents stdin >>= C.putStrLn
-    [file] -> C.hGetContents stdin >>= C.writeFile file
+    []     -> C.hGetContents stdin >>= C.putStrLn . decode
+    [file] -> C.hGetContents stdin >>= C.writeFile file . decode
     args   -> error $ "invalid arguments: " <> unwords args
+
+decode :: ByteString -> ByteString
+decode = C.pack . QuotedPrintable.decode . C.unpack
