efb8c941405feb638750a5c68283e7d0f8eceab4
Bryan Richter <b@chreekat.net>
2015-02-20 11:12:51 -0800

    Add wiki tests with unestablished user

--- tests/WikiTest.hs
+++ tests/WikiTest.hs
@@ -12,9 +12,21 @@ wikiSpecs :: Spec
 wikiSpecs =
     ydescribe "wiki" $ do
 
+        yit "Unestablished user can't create" $ [marked|
+            loginAs Sue
+            get $ NewWikiR snowdrift LangEn "wiki-no-dice"
+            statusIs 403
+        |]
+
         yit "creates a new page" $ [marked|
             loginAs TestUser
             newWiki snowdrift LangEn "wiki-test" "wiki test: new page"
+        |]
+
+        yit "Unestablished user can't edit" $ [marked|
+            loginAs Sue
+            get $ EditWikiR snowdrift LangEn "wiki-test"
+            statusIs 403
         |]
 
         yit "edits a wiki page" $ [marked|
