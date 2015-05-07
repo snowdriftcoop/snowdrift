4d72a6c6ca02eed352efca0520896c9fadc197e0
Bryan Richter <b@chreekat.net>
2015-05-06 20:56:07 -0700

    Fix long lines of Model.Issue

--- Model/Issue.hs
+++ Model/Issue.hs
@@ -21,7 +21,8 @@ class Issue a where
     issueOrderable  :: a -> Orderable
 
 -- Existentially quantified Issue.
-newtype SomeIssue = SomeIssue { unSomeIssue :: forall b. (forall a. Issue a => a -> b) -> b }
+newtype SomeIssue =
+    SomeIssue { unSomeIssue :: forall b. (forall a. Issue a => a -> b) -> b }
 
 mkSomeIssue :: Issue a => a -> SomeIssue
 mkSomeIssue issue = SomeIssue (\k -> k issue)
@@ -62,24 +63,38 @@ instance Issue GH.Issue where
             |]
 
         fg :: String -> String
-        fg = printf "%06x" . pickForegroundColor . maybe 0 fst . listToMaybe . readHex
+        fg = printf "%06x"
+             . pickForegroundColor
+             . maybe 0 fst
+             . listToMaybe
+             . readHex
     issueFilterable = mkFromGithubIssue Filterable
     issueOrderable = mkFromGithubIssue Orderable
 
-mkFromGithubIssue :: ((Text -> Bool) -> (Text -> Bool) -> (Text -> Set UTCTime) -> (Text -> Bool) -> t) -> GH.Issue -> t
+mkFromGithubIssue
+    :: ((Text -> Bool)
+        -> (Text -> Bool)
+        -> (Text -> Set UTCTime)
+        -> (Text -> Bool)
+        -> t)
+    -> GH.Issue
+    -> t
 mkFromGithubIssue c i = c is_claimed has_tag get_named_ts search_literal
   where
     has_issue_assignee = isJust $ GH.issueAssignee i
     is_claimed "CLAIMED"   = has_issue_assignee
-    is_claimed "UNCLAIMED" = has_issue_assignee  -- inverted in 'Data.Filter' and 'Data.Order'
+    -- | inverted in 'Data.Filter' and 'Data.Order'
+    is_claimed "UNCLAIMED" = has_issue_assignee
     is_claimed cmd         = error $ "Unrecognized command " <> T.unpack cmd
 
     has_tag t = elem (T.unpack t) $ map GH.labelName $ GH.issueLabels i
 
-    get_named_ts "CREATED" = S.singleton $ GH.fromGithubDate $ GH.issueCreatedAt i
-    get_named_ts "LAST UPDATED" = S.singleton $ GH.fromGithubDate $ GH.issueUpdatedAt i
+    get_named_ts "CREATED" =
+        S.singleton $ GH.fromGithubDate $ GH.issueCreatedAt i
+    get_named_ts "LAST UPDATED" =
+        S.singleton $ GH.fromGithubDate $ GH.issueUpdatedAt i
     get_named_ts name = error $ "Unrecognized time name " ++ T.unpack name
 
     search_literal str =
-            not (null $ T.breakOnAll str $ T.pack $ GH.issueTitle i)
-                    || fromMaybe False (null . T.breakOnAll str . T.pack <$> GH.issueBody i)
+        not (null $ T.breakOnAll str $ T.pack $ GH.issueTitle i)
+        || fromMaybe False (null . T.breakOnAll str . T.pack <$> GH.issueBody i)
