278d673a327c84c250c338cc7f1d9c2b5132d4d2
Bryan Richter <b@chreekat.net>
2015-05-07 14:35:58 -0700

    Fix long lines of Model.Permission.Internal

--- Model/Permission/Internal.hs
+++ Model/Permission/Internal.hs
@@ -11,11 +11,13 @@ import Data.Text as T
 
 import Debug.Trace
 
-data PermissionLevel = Public | Normal | Moderated deriving (Eq, Show, Read, Ord, Enum, Bounded)
+data PermissionLevel = Public | Normal | Moderated
+    deriving (Eq, Show, Read, Ord, Enum, Bounded)
 
 derivePersistField "PermissionLevel"
 
 instance PathPiece PermissionLevel where
-    fromPathPiece s = if T.null s then Nothing else Just (read $ traceShow s $ T.unpack s)
+    fromPathPiece s =
+        if T.null s then Nothing else Just (read $ traceShow s $ T.unpack s)
     toPathPiece = T.pack . show
 
