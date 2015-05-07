efc6b718bce61ca04dff323ee720019a0aea0fca
Bryan Richter <b@chreekat.net>
2015-05-07 11:53:31 -0700

    Fix long lines of Model.Language.TH
    These "fixes" end up looking ugly, which indicates this file needs
    additional refactoring.

--- Model/Language/TH.hs
+++ Model/Language/TH.hs
@@ -19,28 +19,73 @@ makeLanguages = QuasiQuoter
     , quoteType = fail "no type expansion of langs"
     , quoteDec  =
         words >>> \ langs -> return
-            [ DataD [] language [] (map lang2conC langs) (map mkName $ words "Eq Ord Enum Bounded Show Read Data Typeable")
+            [ DataD
+                []
+                language
+                []
+                (map lang2conC langs)
+                (map mkName
+                     (words "Eq Ord Enum Bounded Show Read Data Typeable"))
 
             , InstanceD [] (AppT (ConT $ mkName "PersistField") (ConT language))
-                [ FunD (mkName "toPersistValue") $ map (\ lang -> Clause [lang2conP lang] (NormalB $ AppE (ConE persistText) $ LitE $ StringL lang) []) langs
-                , FunD (mkName "fromPersistValue")
-                    $ map (\ lang -> Clause [ConP persistText [LitP $ StringL lang]] (NormalB $ rightE $ lang2conE lang) []) langs
-                        ++ [ Clause [WildP] (NormalB $ leftE $ LitE $ StringL "bad persistent type for Language") [] ]
+                [ FunD
+                    (mkName "toPersistValue")
+                    (map (\lang ->
+                            Clause [lang2conP lang]
+                                   (NormalB
+                                        (AppE (ConE persistText)
+                                               (LitE $ StringL lang)))
+                                   [])
+                        langs)
+                , FunD
+                    (mkName "fromPersistValue")
+                    (map (\ lang -> Clause [ConP persistText
+                                                 [LitP $ StringL lang]]
+                                           (NormalB $ rightE $ lang2conE lang)
+                                           [])
+                         langs
+                        ++ [badLangType])
                 ]
 
-            , InstanceD [] (AppT (ConT $ mkName "PathPiece") (ConT language))
-                [ FunD (mkName "toPathPiece") $ map (\ lang -> Clause [lang2conP lang] (NormalB $ LitE $ StringL lang) []) langs
-                , FunD (mkName "fromPathPiece")
-                    $ map (\ lang -> Clause [LitP $ StringL lang] (NormalB $ justE $ lang2conE lang) []) langs
-                        ++ [ Clause [WildP] (NormalB $ ConE (mkName "Nothing")) [] ]
+            , InstanceD
+                []
+                (AppT (ConT $ mkName "PathPiece") (ConT language))
+                [ FunD
+                    (mkName "toPathPiece")
+                    (map (\ lang -> Clause [lang2conP lang]
+                                           (NormalB $ LitE $ StringL lang)
+                                           [])
+                         langs)
+                , FunD
+                    (mkName "fromPathPiece")
+                    (map (\ lang -> Clause [LitP $ StringL lang]
+                                           (NormalB $ justE $ lang2conE lang)
+                                           [])
+                         langs
+                        ++ [ Clause [WildP]
+                                    (NormalB $ ConE (mkName "Nothing"))
+                                    []])
                 ]
 
-            , InstanceD [] (AppT (ConT $ mkName "PersistFieldSql") (ConT language))
-                [ FunD (mkName "sqlType") [Clause [WildP] (NormalB $ ConE (mkName "SqlString")) []] ]
+            , InstanceD
+                []
+                (AppT (ConT $ mkName "PersistFieldSql")
+                      (ConT language))
+                [ FunD
+                    (mkName "sqlType")
+                    [Clause [WildP]
+                            (NormalB $ ConE (mkName "SqlString"))
+                            []]]
 
             ]
     }
   where
+
+    badLangType =
+        Clause [WildP]
+               (NormalB $
+                    leftE $ LitE $ StringL "bad persistent type for Language")
+               []
     langName = uc1 >>> ("Lang" ++) >>> mkName
     lang2conE = langName >>> \ name -> ConE name
     lang2conP = langName >>> \ name -> ConP name []
