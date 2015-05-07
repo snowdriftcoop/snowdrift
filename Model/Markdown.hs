18024914c615100247a5ac19fdd93330925808cb
Bryan Richter <b@chreekat.net>
2015-05-07 13:12:49 -0700

    Fix long lines of Model.Markdown

--- Model/Markdown.hs
+++ Model/Markdown.hs
@@ -13,7 +13,8 @@ import           Text.Pandoc
 import           Yesod.Markdown
 
 
--- TODO: we should probably put together some standard sets of these transforms for use in various places, rather than assembling ad-hoc
+-- TODO: we should probably put together some standard sets of these transforms
+-- for use in various places, rather than assembling ad-hoc
 
 fixLinks :: Text -> DiscussionOn -> Text -> Handler Text
 fixLinks project' discussion_on line' = do
@@ -34,24 +35,30 @@ fixLinks project' discussion_on line' = do
 
         project = encodeUtf8 project'
 
-        expand_match (pre, _, post, matches) = pre <> build_link matches <> parse post
+        expand_match (pre, _, post, matches) =
+            pre <> build_link matches <> parse post
 
         build_link [link, _, comment, "", "", path] =
-            let comment_id :: CommentId
-                comment_id = fromMaybe (error "bad comment id") $ fromPathPiece $ decodeUtf8 comment
+            link <> "(" <> route <> path <> ")"
+          where
+            route = encodeUtf8 $ render $ case discussion_on of
+                DiscussionOnProject (Entity _ Project{..}) ->
+                    ProjectCommentR projectHandle comment_id
+                DiscussionOnWikiPage (Entity _ WikiTarget{..}) ->
+                    WikiCommentR
+                        project'
+                        wikiTargetLanguage
+                        wikiTargetTarget
+                        comment_id
+                DiscussionOnUser (Entity user_id _) ->
+                    UserCommentR user_id comment_id
+                DiscussionOnBlogPost (Entity _ BlogPost{..}) ->
+                    BlogPostCommentR project' blogPostHandle comment_id
 
-             in mconcat
-                    [ link
-                    , "("
-                        , let route = encodeUtf8 $ render $ case discussion_on of
-                                DiscussionOnProject     (Entity _ Project{..})      -> ProjectCommentR projectHandle comment_id
-                                DiscussionOnWikiPage    (Entity _ WikiTarget{..})   -> WikiCommentR project' wikiTargetLanguage wikiTargetTarget comment_id
-                                DiscussionOnUser        (Entity user_id _)          -> UserCommentR user_id comment_id
-                                DiscussionOnBlogPost    (Entity _ BlogPost{..})     -> BlogPostCommentR project' blogPostHandle comment_id
+            comment_id :: CommentId
+            comment_id = fromMaybe (error "bad comment id")
+                                   (fromPathPiece $ decodeUtf8 comment)
 
-                           in route <> path
-                    , ")"
-                    ]
 
         build_link [link, _, "", proj, page, path] =
             mconcat
@@ -62,7 +69,8 @@ fixLinks project' discussion_on line' = do
                 , ")"
                 ]
 
-        build_link [_, content, _, _, _] = error $ "strange match: " <> show content
+        build_link [_, content, _, _, _] =
+            error $ "strange match: " <> show content
         build_link _ = error $ "strange match"
 
         parse str = either error (maybe str expand_match) (regexec pattern str)
@@ -72,40 +80,42 @@ fixLinks project' discussion_on line' = do
 
 
 linkTickets :: Text -> Handler Text
-linkTickets line' = do
-    let Right pattern = compile defaultCompOpt defaultExecOpt "\\<SD-([0-9][0-9]*)" -- TODO word boundaries?
-        getLinkForTicketComment :: TicketId -> Handler (Maybe Text)
-        getLinkForTicketComment ticket_id = do
-            info <- runDB $ select $ from $ \ ticket -> do
-                where_ $ ticket ^. TicketId ==. val ticket_id
+linkTickets line' =
+    fmap decodeUtf8 $ parse line (regexec pattern line)
+  where
+    -- TODO word boundaries?
+    Right pattern = compile defaultCompOpt defaultExecOpt "\\<SD-([0-9][0-9]*)"
+    getLinkForTicketComment :: TicketId -> Handler (Maybe Text)
+    getLinkForTicketComment ticket_id = do
+        info <- runDB $ select $ from $ \ ticket -> do
+            where_ $ ticket ^. TicketId ==. val ticket_id
 
-                return $ ticket ^. TicketComment
+            return $ ticket ^. TicketComment
 
-            case map (toPersistValue . unwrapValues) info of
-                [] -> return Nothing
-                ((PersistInt64 comment_id) : _) -> return $ Just $ mconcat
-                    [ "/c/",  T.pack (show comment_id) ]
+        case map (toPersistValue . unwrapValues) info of
+            [] -> return Nothing
+            ((PersistInt64 comment_id) : _) -> return $ Just $ mconcat
+                [ "/c/",  T.pack (show comment_id) ]
 
-                _ -> error "Unexpected result for ticket reference"
+            _ -> error "Unexpected result for ticket reference"
 
-        parse _   (Left err) = error err
-        parse str (Right Nothing) = return str
-        parse _   (Right (Just (pre, _, post, [ticket_number]))) = do
-            maybe_link <- getLinkForTicketComment $ key $
-                PersistInt64 $ read $ T.unpack $ decodeUtf8 ticket_number
-            rest <- parse post (regexec pattern post)
-            return $ mconcat
-                [ pre
-                , "[SD-" <> ticket_number <> "]"
-                , case maybe_link of
-                    Nothing -> ""
-                    Just link -> mconcat [ "(", encodeUtf8 link, ")" ]
-                ] <> rest
+    parse _   (Left err) = error err
+    parse str (Right Nothing) = return str
+    parse _   (Right (Just (pre, _, post, [ticket_number]))) = do
+        maybe_link <- getLinkForTicketComment $ key $
+            PersistInt64 $ read $ T.unpack $ decodeUtf8 ticket_number
+        rest <- parse post (regexec pattern post)
+        return $ mconcat
+            [ pre
+            , "[SD-" <> ticket_number <> "]"
+            , case maybe_link of
+                Nothing -> ""
+                Just link -> mconcat [ "(", encodeUtf8 link, ")" ]
+            ] <> rest
 
-        parse _ (Right (Just _)) = error "strange match"
+    parse _ (Right (Just _)) = error "strange match"
 
-        line = encodeUtf8 line'
-     in fmap decodeUtf8 $ parse line (regexec pattern line)
+    line = encodeUtf8 line'
 
 renderMarkdown :: Markdown -> Handler Html
 renderMarkdown = renderMarkdownWith return
@@ -151,10 +161,12 @@ fixTests = [minBound .. maxBound] >>= \case
         )]
 
 testFixLinks :: Handler [(DiscussionOn, Text, Text, Text)]
-testFixLinks = do
-    fmap (concat . concat) $ forM fixTests $ \ (discussion, examples) -> forM examples $ \ (input, output) -> do
-        output' <- fixLinks "project" discussion input
-        if output == output'
-         then return []
-         else return [(discussion, input, output, output')]
-
+testFixLinks =
+    fmap (concat . concat)
+         (forM fixTests $ \ (discussion, examples) ->
+          forM examples $ \ (input, output) -> do
+            output' <- fixLinks "project" discussion input
+            return $
+                if output == output'
+                then []
+                else [(discussion, input, output, output')])
