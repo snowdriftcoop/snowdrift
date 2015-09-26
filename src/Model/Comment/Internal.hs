0a4bcbb5766f5771a22e20b82b3f28dffe6fe8b6
Aaron Wolf <wolftune@gmail.com>
2015-09-25 21:48:08 -0700

    added AGF & misrepresent to flagging per CoC
    I updated the CoC in the wiki to account for a couple factors that were
    missing and so added those data points to the flagging options

--- src/Model/Comment/Internal.hs
+++ src/Model/Comment/Internal.hs
@@ -25,23 +25,27 @@ data NewComment = NewComment Markdown Visibility Language
 data EditComment = EditComment Markdown Language
 
 data FlagReason
-    = FlagPersonalAttack
+    = FlagViolateAGF
+    | FlagPersonalAttack
     | FlagUnconstructiveCriticism
     | FlagCondescension
     | FlagDefensiveness
     | FlagSpamming
     | FlagPrivacyViolation
+    | FlagMisrepresentation
     | FlagHateSpeech
     deriving (Bounded, Enum, Eq, Read, Show)
 derivePersistField "FlagReason"
 
 descFlagReason :: FlagReason -> Text
+descFlagReason FlagViolateAGF              = "Assumption of bad faith"
 descFlagReason FlagPersonalAttack          = "Personal attack"
 descFlagReason FlagUnconstructiveCriticism = "Unconstructive criticism"
 descFlagReason FlagCondescension           = "Condescension"
 descFlagReason FlagDefensiveness           = "Defensiveness"
 descFlagReason FlagSpamming                = "Spamming"
 descFlagReason FlagPrivacyViolation        = "Privacy violation"
+descFlagReason FlagMisrepresentation       = "Misrepresentation"
 descFlagReason FlagHateSpeech              = "Hate speech"
 
 newtype TicketName = TicketName { unTicketName :: Text } deriving Eq
