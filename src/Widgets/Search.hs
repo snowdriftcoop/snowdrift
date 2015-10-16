1847fec4a805d60e62bdf912505e8897f9f267ea
Jason Harrer <jazzyeagle79@gmail.com>
2015-10-16 09:25:12 -0600

    All fields now functional.  Additional work needed in the aesthetics department.

--- src/Widgets/Search.hs
+++ src/Widgets/Search.hs
@@ -8,21 +8,78 @@ import Debug.Trace
 
 data FilterClaimStatus = Claimed | Unclaimed | All deriving (Show, Eq)
 
-data TagStatus = Included | Excluded | Neither deriving (Eq)
+data DateTypes = DateTypeBlank | DateCreated | DateLastUpdated deriving Eq
+data DateParams = DateParamBlank | DateBefore | DateAfter | DateBetween deriving Eq
+data DateStatus = DateType DateTypes | DateParam DateParams | DateOther Text deriving Eq
+data TagStatus = Included | Excluded | Neither deriving Eq
 
-data TimeParameters = TimeParameters
-    { basedON :: Text
-    , criteria :: Text
-    , startTime :: Text
-    , endTime :: Text
+data DateParameters = DateParameters
+    { basedON :: DateTypes
+    , criteria :: DateParams
+    , startDate :: Text
+    , endDate :: Text
     }
 
 data SearchParameters = SearchParameters
     { claimed :: FilterClaimStatus
-    , time :: TimeParameters
+    , date :: DateParameters
     , tags :: [(Text, Text)]
     , sort :: Maybe Text
     }
+
+fromDateType :: DateTypes -> Text
+fromDateType x = case x of
+               DateTypeBlank -> ""
+               DateCreated -> "Created"
+               DateLastUpdated -> "Last Updated"
+
+toDateType :: Text -> DateTypes
+toDateType x = case x of
+                "Created" -> DateCreated
+                "Last Updated" -> DateLastUpdated
+                _ -> DateTypeBlank
+
+fromDateParam :: DateParams -> Text
+fromDateParam x = case x of
+                DateParamBlank -> ""
+                DateBefore -> "before"
+                DateAfter -> "after"
+                DateBetween -> "between"
+
+toDateParam :: Text -> DateParams
+toDateParam x = case x of
+                  "before" -> DateBefore
+                  "after" -> DateAfter
+                  "between" -> DateBetween
+                  _ -> DateParamBlank
+
+textFromDateStatus :: DateStatus -> Text
+textFromDateStatus x = case x of
+                        DateType a -> fromDateType a
+                        DateParam b -> fromDateParam b
+                        DateOther c -> c
+
+dateStatus :: Text -> DateParameters -> DateStatus
+dateStatus tid responses = 
+        case tid of
+            "datetype"  -> DateType $ basedON responses
+            "dateparam" -> DateParam $ criteria responses
+            "datestart" -> DateOther $ startDate responses
+            "dateend"   -> DateOther $ endDate responses
+--dateStatus :: Text -> [(Text, Text)] -> DateStatus
+--dateStatus tid responses =
+--    case tid of
+--        "datetype" -> case (fromMaybe "" $ lookup tid responses) of
+--                        "" -> DateType DateTypeBlank
+--                        "Created" -> DateType DateCreated
+--                        "Last Updated" -> DateType DateLastUpdated
+--        "dateparam" -> case (fromMaybe "" $ lookup tid responses) of
+--                        "" -> DateParam DateParamBlank
+--                        "before" -> DateParam DateBefore
+--                        "after" -> DateParam DateAfter
+--                        "between" -> DateParam DateBetween
+--        "datestart" -> DateOther $ fromMaybe "" $ lookup tid responses
+--        "dateend" -> DateOther $ fromMaybe "" $ lookup tid responses
 
 tagStatus :: Text -> [(Text, Text)] -> TagStatus
 tagStatus tag responses =
@@ -41,14 +98,13 @@ searchForm tags extra = do
         (radioFieldList claimedList)
         "Claimed"
         (Just All)
---    (timeRes, timeView) <- mreq timeField "Time" Nothing
+    (dateRes, dateView) <- mreq dateField "Time" Nothing
     (tagsRes, tagsView) <- mreq (tagField tags) "Tags" Nothing
     (sortRes, sortView) <- mopt textField "Sort" Nothing
 
     let searchRes = SearchParameters 
                     <$> claimedRes
-                    <*> pure (TimeParameters "" "" "" "")
---                    <*> timeRes
+                    <*> dateRes
                     <*> tagsRes
                     <*> sortRes
 
@@ -57,6 +113,7 @@ searchForm tags extra = do
             #{extra}
             <div>
               <p>^{fvInput claimedView}
+              <p>^{fvInput dateView}
               <p>tags:
               ^{fvInput tagsView}
             <div>
@@ -65,59 +122,67 @@ searchForm tags extra = do
 
     return (searchRes, widget)
 
-timeField :: Field Handler TimeParameters
-timeField = Field
-    { fieldParse = \timeSelectValues _ ->
-        trace ("timeWidget fieldParse\nlength of timeSelectValues: " ++ (show (length timeSelectValues)))
-        (case timeSelectValues of
-            [a, b, c, d] -> (return $ Right $ Just $ TimeParameters a b c d)
-            _ -> return $ Left "Error with time selection: Missing/Too Much input.")
-    , fieldView = \fieldId fieldName fieldAttrs fieldVal fieldReq -> $(widgetFile "time_widget")
+dateField :: Field Handler DateParameters
+dateField = Field
+    { fieldParse = \dateSelectValues _ ->
+--        trace ("timeWidget fieldParse\nlength of timeSelectValues: " ++ (show (length timeSelectValues)))
+        case dateSelectValues of
+            --a -> return $ Right $ Just $ a
+            [a, b, c, d] -> trace 
+                                ("Date trace:  " ++ (show a) ++ " " ++ (show b) ++ " " ++ (show c) ++ " " ++ (show d) ++ "\n")
+                                (return $ Right $ Just (DateParameters (toDateType a) (toDateParam b) (c :: Text) (d :: Text)))
+            _ -> return $ Left "Error with time selection: Missing/Too Much input."
+    , fieldView = \_ fieldName _ fieldVal _ -> $(widgetFile "filter_datefield")
     , fieldEnctype = UrlEncoded
     }
 
 tagField :: [Text] -> Field Handler [(Text, Text)]
 tagField tags = Field
-    { fieldParse = \tagSelectValues _ ->
-          trace ("tagWidget fieldParse\nlength of tagSelectValues: " ++ (show (length tagSelectValues)))
-            (if (length tagSelectValues > 0) then
-                traceShow (zip tags tagSelectValues)
-                (return $ Right $ Just $ zip tags tagSelectValues)
-             else
-                return $ Left "Error with tag selection: No input.")
-    , fieldView = \fieldId fieldName fieldAttrs fieldVal fieldReq -> $(widgetFile "tag_widget")
+    { fieldParse = \tagSelectValues _ -> return $ Right $ Just $ zip tags tagSelectValues
+--          trace ("tagWidget fieldParse\nlength of tagSelectValues: " ++ (show (length tagSelectValues)))
+--            (if (length tagSelectValues > 0) then
+--                traceShow (zip tags tagSelectValues)
+--                (return $ Right $ Just $ zip tags tagSelectValues)
+--             else
+--                return $ Left "Error with tag selection: No input.")
+    , fieldView = \_ fieldName _ fieldVal _ -> $(widgetFile "filter_tagfield")
     , fieldEnctype = UrlEncoded
     }
 
 searchFilterString :: SearchParameters -> Text
-searchFilterString (SearchParameters Claimed time tags _ ) = 
+searchFilterString (SearchParameters Claimed searchdate tags _ ) = 
         T.intercalate " AND " $ filter (not . T.null)
             [(T.pack "CLAIMED"),
-             (parseTime time),
+             (parseFilterDate searchdate),
              (parseTags tags)
             ]
-        -- T.append "CLAIMED AND " $ parseTags tags
-searchFilterString (SearchParameters Unclaimed time tags _ ) =
+searchFilterString (SearchParameters Unclaimed searchdate tags _ ) =
         T.intercalate " AND " $ filter (not . T.null)
             [(T.pack "UNCLAIMED"),
-             (parseTime time),
+             (parseFilterDate searchdate),
              (parseTags tags)
             ]
-        --T.append "UNCLAIMED AND " $ parseTags tags
-searchFilterString (SearchParameters All time tags _) =
+searchFilterString (SearchParameters All searchdate tags _) =
         T.intercalate " AND " $ filter (not . T.null)
-            [(parseTime time),
+            [(parseFilterDate searchdate),
              (parseTags tags)
             ]
-        --T.append T.empty $ parseTags tags
 
-parseTime :: TimeParameters -> Text
-parseTime (TimeParameters base crit start end)
-        | (base == "Created") || (base == "Last Updated") = T.toUpper $
-                if (crit == "between") then
-                    T.intercalate " " [base, crit, start, "AND", end]
+parseFilterDate :: DateParameters -> Text
+parseFilterDate (DateParameters base crit start end)
+        | (base == DateCreated) || (base == DateLastUpdated) = T.toUpper $
+                if (crit == DateBetween) then
+                    T.intercalate " " 
+                        [(fromDateType base), 
+                         (fromDateParam crit), 
+                         start, 
+                         "AND", 
+                         end]
                 else
-                    T.intercalate " " [base, crit, start]
+                    T.intercalate " "
+                        [(fromDateType base),
+                         (fromDateParam crit),
+                         start]
         | otherwise = T.empty
 
 parseTags :: [(Text, Text)] -> Text
