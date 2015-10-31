790ae6e5ca6a89a5ec986c69238911855facc24e
Bryan Richter <b@chreekat.net>
2015-10-30 17:49:48 -0700

    Proactively remove mech stuff from the navbar
    mray has already removed it on his design-relanch.

--- original/src/Widgets/Navbar.hs
+++ original/src/Widgets/Navbar.hs
@@ -3,35 +3,16 @@ module Widgets.Navbar where
 
 import Import
 
-import Mechanism
 import Model.User
 
 navbar :: Widget
 navbar = do
     maybe_user <- handlerToWidget maybeAuth
-
     alreadyExpired
 
-    money_info <- handlerToWidget (runDB (moneyInfo maybe_user))
-    (num_unread_notifs, pledgeStat) <- case maybe_user of
-        Nothing -> return (0, AllFunded)
-        Just (Entity user_id _) -> do
-            (num_unread_notifs, pledgeStat) <-
-                handlerToWidget $ runDB $ goDB user_id
-
-            return (num_unread_notifs, pledgeStat)
-
-    let hasUnderfunded = case pledgeStat of
-            ExistsUnderfunded -> True
-            AllFunded -> False
-
+    num_unread_notifs <- case maybe_user of
+        Nothing -> return 0
+        Just (Entity user_id _) ->
+            handlerToWidget
+                (runDB (fetchNumUnreadNotificationsDB  user_id))
     $(widgetFile "navbar")
-
-  where
-
-    goDB user_id = do
-        num_unread_notifs <- fetchNumUnreadNotificationsDB user_id
-        pledgeStat <- pledgeStatus user_id
-        return
-            ( num_unread_notifs
-            , pledgeStat)
