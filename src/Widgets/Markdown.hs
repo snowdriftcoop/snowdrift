bf004e24f284e972d79c0a443cfd2e97fa45e892
Bryan Richter <b@chreekat.net>
2015-11-24 15:41:01 -0800

    Remove unnecessary pre-render of url

--- src/Widgets/Markdown.hs
+++ src/Widgets/Markdown.hs
@@ -8,20 +8,18 @@ snowdriftMarkdownField :: (Monad m, HandlerSite m ~ App) => Field m Markdown
 snowdriftMarkdownField = Field
     { fieldParse = parseHelper $ Right . Markdown . T.filter (/= '\r')
     , fieldView  = \theId name attrs value _isReq -> do
-            render <- getUrlRender
-            let tutorial = render MarkdownTutorialR in
-                [whamlet|
-                    <div .markdown_label>
-                        Use
-                        <a href=#{tutorial} target="_blank">
-                            Markdown syntax
-                    <div .markdown_label>
-                        Remember to follow the
-                        <a href=@{WikiR "snowdrift" LangEn "conduct"}>
-                            Code of Conduct
-                    <div .markdown_wrapper>
-                        <textarea id=#{theId} name=#{name} :_isReq:required *{attrs}>#{either id unMarkdown value}
-                |]
+        [whamlet|
+            <div .markdown_label>
+                Use
+                <a href=@{MarkdownTutorialR} target="_blank">
+                    Markdown syntax
+            <div .markdown_label>
+                Remember to follow the
+                <a href=@{WikiR "snowdrift" LangEn "conduct"}>
+                    Code of Conduct
+            <div .markdown_wrapper>
+                <textarea id=#{theId} name=#{name} :_isReq:required *{attrs}>#{either id unMarkdown value}
+        |]
 
     , fieldEnctype = UrlEncoded
     }
