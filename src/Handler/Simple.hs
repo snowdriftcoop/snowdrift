f2f6b017418a3d9c9703a9e74a445e3d2d357023
Aaron Wolf <wolftune@gmail.com>
2015-12-19 22:53:25 -0800

    add stub page for assets

--- src/Handler/Simple.hs
+++ src/Handler/Simple.hs
@@ -36,6 +36,7 @@ getTeamR               = $(simpleHandler "team" "Team")
 getPressR              = $(simpleHandler "press" "Press")
 getSponsorsR           = $(simpleHandler "sponsors" "Sponsors")
 getDonateR             = $(simpleHandler "donate" "Donate")
+getAssetsR             = $(simpleHandler "assets" "Assets")
 
 -- * TODO: Convert these.
 getTermsR,
