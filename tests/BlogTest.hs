6f8f390d868d3f8f029c8908abb69cba011c9583
Bryan Richter <b@chreekat.net>
2016-01-06 15:06:20 -0800

    Refactor

--- tests/BlogTest.hs
+++ tests/BlogTest.hs
@@ -14,38 +14,8 @@ import qualified Text.HTML.DOM as HTML
 
 blogSpecs :: Spec
 blogSpecs = do
-    let postBlog route stmts = [marked|
-            get200 route
-
-            [ form ] <- htmlQuery "form"
-
-            let getAttrs = XML.elementAttributes . XML.documentRoot . HTML.parseLBS
-
-            withStatus 303 True $ request $ do
-                addToken
-                setMethod "POST"
-                let route' = maybe (Left route) Right $ M.lookup "action" $ getAttrs form
-                either setUrl setUrl route'
-                addPostParam "mode" "post"
-                stmts
-        |]
-
-        previewBlog route stmts = [marked|
-            get200 route
-
-            [ form ] <- htmlQuery "form"
-
-            let getAttrs = XML.elementAttributes . XML.documentRoot . HTML.parseLBS
-
-            withStatus 200 False $ request $ do
-                addToken
-                setMethod "POST"
-                maybe (setUrl route) setUrl $ M.lookup "action" $ getAttrs form
-
-                addPostParam "mode" "preview"
-                stmts
-        |]
-
+    let getAttrs = XML.elementAttributes . XML.documentRoot . HTML.parseLBS
+        blogRoute = NewBlogPostR "snowdrift"
     ydescribe "blog" $ do
         yit "previews blog post" $ [marked|
             loginAs AdminUser
@@ -54,7 +24,16 @@ blogSpecs = do
             htmlNoneContain ".blog-post" "Above fold."
             htmlNoneContain ".blog-post" "Below fold."
 
-            previewBlog (NewBlogPostR "snowdrift") $ do
+            get200 blogRoute
+
+            [ form ] <- htmlQuery "form"
+
+            withStatus 200 False $ request $ do
+                addToken
+                setMethod "POST"
+                maybe (setUrl blogRoute) setUrl $ M.lookup "action" $ getAttrs form
+
+                addPostParam "mode" "preview"
                 byLabel "Title for this blog post" "Test"
                 byLabel "Handle for the URL" "test"
                 byLabel "Content" "Above fold.\n***\nBelow fold."
@@ -70,7 +49,16 @@ blogSpecs = do
             htmlNoneContain ".blog-post" "Above fold."
             htmlNoneContain ".blog-post" "Below fold."
 
-            postBlog (NewBlogPostR "snowdrift") $ do
+            get200 blogRoute
+
+            [ form ] <- htmlQuery "form"
+
+            withStatus 303 True $ request $ do
+                addToken
+                setMethod "POST"
+                let route' = maybe (Left blogRoute) Right $ M.lookup "action" $ getAttrs form
+                either setUrl setUrl route'
+                addPostParam "mode" "post"
                 byLabel "Title for this blog post" "Test"
                 byLabel "Handle for the URL" "test"
                 byLabel "Content" "Above fold.\n***\nBelow fold."
@@ -78,12 +66,10 @@ blogSpecs = do
             Just route <- extractLocation
 
             get200 $ decodeUtf8 route
-
             htmlAnyContain ".blog-post-top" "Above fold."
             htmlNoneContain ".blog-post-top" "Below fold."
 
             get $ BlogPostR "snowdrift" "test"
-
             htmlAnyContain ".blog-post" "Above fold."
             htmlAnyContain ".blog-post" "Below fold."
 
