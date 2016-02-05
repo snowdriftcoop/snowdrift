9ac56d401d5669214c59e817685f47a545e5be67
Michael Siepmann <MS@TechDesignPsych.com>
2016-02-05 12:56:17 -0700

    A few text changes to the volunteer form - both to the introductory text above it and to two field labels.

--- src/Handler/Volunteer.hs
+++ src/Handler/Volunteer.hs
@@ -10,12 +10,12 @@ volunteerForm :: UTCTime -> ProjectId -> [Entity Interest] -> Entity User -> For
 volunteerForm now project_id interests (Entity user_id user) = renderBootstrap3 BootstrapBasicForm $
     (\name email contact website location experience interest_ids other ->
         (VolunteerApplication now project_id user_id name email contact website location experience other, interest_ids)
-    )   <$> areq' textField "Name or Internet Handle:" Nothing
+    )   <$> areq' textField "Name and/or Internet Handle:" Nothing
         <*> areq' emailField "E-mail:" (Just . userIdent $ user)
         <*> aopt' textField "Other contact info (phone, IRC handle, chat ID, etc):" Nothing
         <*> aopt' textField "Website URL:" Nothing
         <*> aopt' textField "Location:" Nothing
-        <*> aopt' textareaField "Relevant work/training/volunteer experience:" Nothing
+        <*> aopt' textareaField "Relevant work/training/volunteer experience (Feel free to either briefly summarize relevant aspects of your experience, or to provide more detail, for example by copying and pasting sections from your resume):" Nothing
         <*> areq' (multiSelectFieldList $ (interestDescription . entityVal &&& entityKey) <$> interests)
                     "Areas of interest (use ctrl to select multiple):" Nothing
         <*> aopt' textareaField "Anything else you'd like us to know:" Nothing
