979bd1b68b17b6611eaadaeb53f22b916d029db0
Bryan Richter <b@chreekat.net>
2016-03-18 12:06:11 -0700

    Remove some unused modules

--- src/Model/License.hs
+++
@@ -1,9 +0,0 @@
-module Model.License where
-
-import Import
-
-fetchLicensesDB :: DB [License]
-fetchLicensesDB =
-    fmap (map entityVal) $
-    select $ from $ \(l :: SqlExpr (Entity License)) ->
-    return l
