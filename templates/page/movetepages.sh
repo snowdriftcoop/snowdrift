524e6170d3ccba094d8fb4de6ce98507f0033184
Bryan Richter <b@chreekat.net>
2016-03-24 14:34:17 -0700

    Accidentally committed

--- templates/page/movetepages.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p how-it-works
-git mv intro.*               how-it-works
-git mv flo.*                 how-it-works
-git mv network-effect.*      how-it-works
-git mv sustainable-funding.* how-it-works
-git mv co-op.*               how-it-works
-mkdir about
-git mv old-homepage.* about
