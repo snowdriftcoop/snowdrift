63acd93e4ee9a942b89127b95dc7ef476cc424f9
Bryan Richter <b@chreekat.net>
2016-04-05 19:05:18 -0700

    Remove useless baggage

--- website/src/Application.hs
+++ website/src/Application.hs
@@ -34,7 +34,6 @@ import System.Log.FastLogger                (defaultBufSize, newStdoutLoggerSet,
 -- Don't forget to add new modules to your cabal file!
 import Handler.Common
 import Handler.Home
-import Handler.Comment
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
