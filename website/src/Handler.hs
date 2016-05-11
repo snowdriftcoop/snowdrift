28dc57a9d97d2103c22f7a976fb4dee59299688a
Bryan Richter <b@chreekat.net>
2016-05-11 13:15:49 -0700

    Add hard-coded Snowdrift project

--- website/src/Handler.hs
+++ website/src/Handler.hs
@@ -68,3 +68,7 @@ getJsLicensesR = $(widget "page/js-licenses" "getJsLicensesR")
 
 getMerchandiseR :: Handler Html
 getMerchandiseR = $(widget "page/merchandise" "getMerchandiseR")
+
+-- For MVP, there is one, hard-coded project: Snowdrift
+getSnowdriftProjectR :: Handler Html
+getSnowdriftProjectR = $(widget "page/snowdrift-project" "Snowdrift.coop Project")
