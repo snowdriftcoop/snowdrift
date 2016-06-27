43f06c944575cce6fc12051f6fffee0e5d8d8911
Bryan Richter <b@chreekat.net>
2016-06-27 13:40:14 +0300

    Add loginDest parameter

--- website/src/Application.hs
+++ website/src/Application.hs
@@ -52,6 +52,7 @@ makeFoundation appSettings = do
         (if appMutableStatic appSettings then staticDevel else static)
         (appStaticDir appSettings)
 
+    let appAuth = AuthSite HomeR
     -- We need a log function to create a connection pool. We need a connection
     -- pool to create our foundation. And we need our foundation to get a
     -- logging function. To get out of this loop, we initially create a
--- website/src/AuthSiteTypes.hs
+++ website/src/AuthSiteTypes.hs
@@ -3,7 +3,9 @@ module AuthSiteTypes where
 
 import Yesod.Core
 
-data AuthSite = AuthSite
+-- | The loginDest parameter is the route that users should be directed to
+-- after logging in.
+data AuthSite loginDest = AuthSite loginDest
 
 mkYesodSubData "AuthSite" [parseRoutes|
 /login LoginR GET POST
