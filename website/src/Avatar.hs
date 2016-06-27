ed20ad771d94c69146d594b2f87d5ff7e7b462d3
Bryan Richter <b@chreekat.net>
2016-06-27 13:43:02 +0300

    Use lenses; assume all emails to be verified
    Emails only get stored if they're verified.

--- website/src/Avatar.hs
+++ website/src/Avatar.hs
@@ -1,7 +1,8 @@
 module Avatar where
 
-import qualified Data.Text as T
+import Control.Lens
 import Network.Libravatar
+import qualified Data.Text as T
 
 import Import.NoFoundation
 
@@ -11,10 +12,7 @@ getUserAvatar defaultRoute muser = do
 
     maybe (return defaultUrl)
           (\user -> do
-              let email = userEmail user
-              if userEmailVerified user
-                  then liftIO (libravatar email defaultUrl)
-                  else return defaultUrl)
+              liftIO (libravatar (user^.userEmail) defaultUrl))
           muser
 
   where
