2dbc88eed333dcf5f6ec96782e1d9de7204be38e
Bryan Richter <b@chreekat.net>
2016-07-04 14:52:43 +0300

    Add ProvisionalUser, which goes in the db

--- website/src/AuthSiteTypes.hs
+++ website/src/AuthSiteTypes.hs
@@ -1,6 +1,9 @@
 -- | Core definitions for the Auth subsite (see AuthSite.hs)
 module AuthSiteTypes where
 
+import ClassyPrelude
+
+import Database.Persist.TH
 import Yesod.Core
 
 -- | The loginDest parameter is the route that users should be directed to
@@ -10,4 +13,14 @@ data AuthSite loginDest = AuthSite loginDest
 mkYesodSubData "AuthSite" [parseRoutes|
 /login LoginR GET POST
 /logout LogoutR POST
+|]
+
+share [mkPersist sqlSettings{mpsPrefixFields = False}
+      , mkMigrate "migrateAuth"
+      ] [persistLowerCase|
+ProvisionalUser
+    provisionalEmail Text
+    provisionalDigest ByteString
+    verificationDigest ByteString
+    puCreationTime UTCTime
 |]
