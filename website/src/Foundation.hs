989d24f4e631f3fab4f07f774bf5c5b4cb8a2893
Bryan Richter <b@chreekat.net>
2016-07-20 15:18:43 +0300

    Split out App type-generation

--- website/src/Foundation.hs
+++ website/src/Foundation.hs
@@ -12,38 +12,11 @@ import qualified Data.Text as T
 import qualified Yesod.Core.Unsafe as Unsafe
 
 import Alerts (getAlert)
+import AppTypes
 import Avatar
 import Email
 import Network.Mail.Mime
 import qualified TestHooks
-
--- | The foundation datatype for your application. This can be a good place
--- to keep settings and values requiring initialization before your
--- application starts running, such as database connections. Every handler
--- will have access to the data present here.
-data App = App
-    { appSettings    :: AppSettings
-    , appStatic      :: Static -- ^ Settings for static file serving.
-    , appConnPool    :: ConnectionPool -- ^ Database connection pool.
-    , appHttpManager :: Manager
-    , appLogger      :: Logger
-    , appGitRev      :: GitRev
-    , appAuth        :: AuthSite
-    }
-
--- This is where we define all of the routes in our application. For a full
--- explanation of the syntax, please see:
--- http://www.yesodweb.com/book/routing-and-handlers
---
--- Note that this is really half the story; in Application.hs, mkYesodDispatch
--- generates the rest of the code. Please see the following documentation
--- for an explanation for this split:
--- http://www.yesodweb.com/book/scaffolding-and-the-site-template#scaffolding-and-the-site-template_foundation_and_application_modules
---
--- This function also generates the following type synonyms:
--- type Handler = HandlerT App IO
--- type Widget = WidgetT App IO ()
-mkYesodData "App" $(parseRoutesFile "config/routes")
 
 -- | A convenient synonym for creating forms.
 type Form x = Html -> MForm (HandlerT App IO) (FormResult x, Widget)
--- website/src/Import.hs
+++ website/src/Import.hs
@@ -2,5 +2,6 @@ module Import
     ( module Import
     ) where
 
+import AppTypes              as Import
 import Foundation            as Import
 import Import.NoFoundation   as Import
