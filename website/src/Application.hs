9265641604f32755a724274e1337b3fdc43896b9
Bryan Richter <b@chreekat.net>
2016-07-21 13:10:13 +0300

    Ignore duplication in Application.hs
    Created to ignore the following suggestion:
    
        website/src/Application.hs:125:5: Suggestion: Reduce duplication
        Found:
          settings <- getAppSettings
          foundation <- makeFoundation settings
          wsettings <- getDevSettings $ warpSettings foundation
    
        Why not:
          Combine with website/src/Application.hs:164:5
    
        1 hint

--- website/src/Application.hs
+++ website/src/Application.hs
@@ -33,6 +33,8 @@ import qualified Yesod.GitRev as G
 
 import Handler
 
+{-# ANN module ("HLint: ignore Reduce duplication" :: String) #-}
+
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
 -- comments there for more details.
