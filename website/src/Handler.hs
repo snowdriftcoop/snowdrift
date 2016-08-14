32353eafe605dfe7ec91805c3d2755ce06cd8d2a
Peter Harpending <peter@harpending.org>
2016-08-13 19:14:32 -0600

    Redirect specific old wiki pages to new wiki
    See https://tree.taiga.io/project/snowdrift/task/446
    
    Certain important wiki pages are now redirected to pages on the new
    wiki.

--- website/src/Handler.hs
+++ website/src/Handler.hs
@@ -4,6 +4,7 @@ module Handler where
 import Import
 
 import Data.FileEmbed (embedFile)
+import qualified Data.HashMap.Strict as H
 
 import Handler.TH
 
@@ -73,13 +74,65 @@ getMerchandiseR = $(widget "page/merchandise" "getMerchandiseR")
 getSnowdriftProjectR :: Handler Html
 getSnowdriftProjectR = $(widget "page/snowdrift-project" "Snowdrift.coop Project")
 
--- | Prevents breakage of external links to the old wiki. See
--- https://tree.taiga.io/project/snowdrift/us/359
-getSnowdriftWikiSearchR :: Text -> Handler Html
-getSnowdriftWikiSearchR slug =
-    redirectWith movedPermanently301 $ "https://wiki.snowdrift.coop/_search?patterns=" <> slug
-
 -- | Prevents breakage of external links to the old blog.
 getSnowdriftLegacyBlogR :: Text -> Handler Html
 getSnowdriftLegacyBlogR slug =
     redirectWith movedPermanently301 $ "https://wiki.snowdrift.coop/blog/" <> slug
+
+-- Redirect to new wiki
+getSnowdriftWikiR :: Handler Html
+getSnowdriftWikiR = redirect ("https://wiki.snowdrift.coop/" :: Text)
+
+-- | Prevents breakage of external links to the old wiki. See
+-- https://tree.taiga.io/project/snowdrift/us/359 and
+-- https://tree.taiga.io/project/snowdrift/task/446
+getSnowdriftWikiSearchR :: Text -> Handler Html
+getSnowdriftWikiSearchR slug =
+    redirect $
+        mappend "https://wiki.snowdrift.coop/" $
+            case H.lookup slug routes of
+              Just route -> route
+              Nothing -> "_search?patterns=" <> slug
+  where
+    (.=) = (,)
+    routes = H.fromList $
+        [ "en" .= ""
+        , "about" .= "about"
+        , "conduct" .= "community/conduct"
+        , "economics" .= "about/economics"
+        , "existingmechanisms" .= "about/existing-mechanisms"
+        , "formats-repositories" .= "market-research/flo-repos"
+        , "free-libre-open" .= "about/free-libre-open"
+        , "honor" .= "community/honor"
+        , "honor-projects" .= "project-reqs/honor-projects"
+        , "honor-users" .= "community/honor-users"
+        , "how-to-help" .= "community/how-to-help"
+        , "intro" .= "about/intro"
+        , "irc" .= "community/irc"
+        , "legal" .= "legal"
+        , "licenses" .= "about/licenses"
+        , "limits" .= "about/limits"
+        , "mechanism" .= "about/mechanism"
+        , "mission" .= "about/mission"
+        , "next" .= "planning/next"
+        , "othercrowdfunding" .= "market-research/other-crowdfunding"
+        , "payment-services" .= "market-research/payment-services"
+        , "presentations" .= "about/presentations"
+        , "psychology" .= "about/psychology"
+        , "snowdrift" .= "about/snowdrift-dilemma"
+        , "status-quo-floss" .= "market-research/history/software"
+        , "threshold-systems" .= "about/threshold-systems"
+        , "whyfree" .= "about/why-flo"
+        ]
+
+-- Yesod is being fussy, and won't let me have two URLs go to the same handler
+getSnowdriftWikiEnSearchR :: Text -> Handler Html
+getSnowdriftWikiEnSearchR = getSnowdriftWikiSearchR
+
+-- Redirect /who to community/team
+getLegacyWhoR :: Handler Html
+getLegacyWhoR = redirect ("https://wiki.snowdrift.coop/community/team" :: Text)
+
+-- Redirect /u/3 to community/team
+getLegacyWolftuneR :: Handler Html
+getLegacyWolftuneR = redirect ("https://wiki.snowdrift.coop/community/team/wolftune" :: Text)
