643e10bc39006ddcdcaa453c3bf1a45209c9195a
Bryan Richter <b@chreekat.net>
2016-08-17 11:16:12 +0300

    Prevent ssl downgrade attack
    Also remove unused function from TestHooks

--- website/src/TestHooks.hs
+++ website/src/TestHooks.hs
@@ -1,22 +1,21 @@
 {-# LANGUAGE CPP #-}
 
 -- | TestHooks puts all dicey test-environment overrides in one place.
-module TestHooks (middleware, sendVerifyEmail) where
+module TestHooks (middleware) where
 
 import Import.NoFoundation
 
 -- | Don't use CSRF in testing.
+--
+-- Note that "sslOnlyMiddleware" only prevents downgrades. "HSTS headers
+-- over HTTP are ignored."
 middleware :: (Yesod site, ToTypedContent res)
            => HandlerT site IO res -> HandlerT site IO res
-middleware = addCsrf . defaultYesodMiddleware
+middleware = sslOnlyMiddleware twoHours . addCsrf . defaultYesodMiddleware
   where
+    twoHours = 120
 #if DEVELOPMENT
     addCsrf = id
 #else
     addCsrf = defaultCsrfMiddleware
 #endif
-
--- | Don't actually send email in testing OR development.
--- FIXME: Do send email in production, though :D
-sendVerifyEmail :: MonadLogger m => Text -> t -> Text -> m ()
-sendVerifyEmail e _k u = $logDebug ("Hit up " <> u <> " to authorize " <> e <> " .")
