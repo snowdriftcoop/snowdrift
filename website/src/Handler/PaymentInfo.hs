8ee0f13380e4385e61bfdcfbac25e01ade3bb647
Bryan Richter <b@chreekat.net>
2016-09-06 17:29:30 +0300

    Brute handle Stripe errors

--- website/src/Handler/PaymentInfo.hs
+++ website/src/Handler/PaymentInfo.hs
@@ -8,9 +8,11 @@ module Handler.PaymentInfo
 
 import Import
 
+import Control.Monad.Logger
 import Text.Julius (rawJS)
 import Stripe
 import Web.Stripe.Customer
+import Web.Stripe.Error
 
 import Alerts
 import Handler.Util
@@ -48,23 +50,47 @@ postPaymentInfoR = do
     ((formResult, _), _) <- runFormPost (paymentForm "")
     case formResult of
         FormSuccess token -> do
-            runDB . storeCustomer uid =<< createCustomer' u token
+            let stripeAction = maybe
+                    createCustomer'
+                    (updateCustomer' . CustomerId)
+                    (_userCustomer u)
+            (runDB . storeCustomer uid <=< stripePaymentInfoHandler)
+                =<< stripeAction u token
             alertSuccess "Payment information stored"
             redirect HomeR
         _ -> do
             alertDanger "There was something wrong with your form submission."
             redirect PaymentInfoR
 
-storeCustomer :: MonadHandler m => Key User -> Customer -> SqlPersistT m ()
-storeCustomer uid customer =
+storeCustomer :: MonadHandler m => Key User -> CustomerId -> SqlPersistT m ()
+storeCustomer uid (CustomerId cid) =
     -- FIXME: Store an event
     update uid [UserCustomer =. Just cid]
-  where (CustomerId cid) = customerId customer
 
 -- | Wrap over Stripe's native 'createCustomer'
-createCustomer' :: User -> PaymentToken -> Handler Customer
-createCustomer' User{..} (PaymentToken tok) = do
-    res <- snowstripe (createCustomer -&- Email _userEmail -&- TokenId tok)
-    case res of
-        Right c -> pure c
-        Left er -> error (show er)
+createCustomer' :: User -> PaymentToken -> Handler (Either StripeError Customer)
+createCustomer' User{..} (PaymentToken tok) =
+    snowstripe (createCustomer -&- Email _userEmail -&- TokenId tok)
+
+-- | Wrap over Stripe's native 'updateCustomer'
+updateCustomer' :: CustomerId
+                -> User
+                -> PaymentToken
+                -> Handler (Either StripeError Customer)
+updateCustomer' cid User{..} (PaymentToken tok) =
+    snowstripe (updateCustomer cid -&- TokenId tok)
+
+-- | The preceding two methods use the same error catching. Pretty bare
+-- bones, but at least there will be a log.
+stripePaymentInfoHandler :: Either StripeError Customer -> Handler CustomerId
+stripePaymentInfoHandler =
+    either
+        (\er -> do
+             alertDanger [shamlet|
+                 We at Snowdrift.coop did the best we could, but Stripe
+                 experienced a problem. Stripe says, "#{errorMsg er}"
+                 ...Maybe try again?
+             |]
+             $logErrorSH er
+             redirect PaymentInfoR)
+        (pure . customerId)
