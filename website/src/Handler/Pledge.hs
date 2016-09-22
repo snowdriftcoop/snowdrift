8ccad9ee5cf32ca914540a5d014c265fbd5994e5
Bryan Richter <b@chreekat.net>
2016-09-22 16:51:41 +0300

    Make a pledge

--- website/src/Handler/Pledge.hs
+++ website/src/Handler/Pledge.hs
@@ -2,5 +2,23 @@ module Handler.Pledge (postPledgeSnowdriftR) where
 
 import Import
 
+import Alerts
+
 postPledgeSnowdriftR :: Handler Html
-postPledgeSnowdriftR = error "Makes a pledge"
+postPledgeSnowdriftR = do
+    Entity uid _ <- requireAuth
+    dbres <- runDB $ do
+        p <- getBy (UniquePledge uid)
+        maybe (pledge' uid) feignHorror p
+    case dbres of
+        Just t -> alertInfo
+            [shamlet|Your pledge that started on #{show t} is still valid.|]
+        Nothing -> alertSuccess "You are now pledged!"
+    redirect SnowdriftProjectR
+  where
+    pledge' uid = do
+        now <- liftIO getCurrentTime
+        insert_ (Pledge uid now)
+        insert_ (PledgeHistory uid now MakePledge)
+        pure Nothing
+    feignHorror (Entity _ Pledge{..}) = pure (Just _pledgeSince)
