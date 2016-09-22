a77cbb20fc673bb8691af7432c193e2900809c09
Bryan Richter <b@chreekat.net>
2016-09-22 19:30:54 +0300

    Add pledge buttons

--- website/src/Handler/Project.hs
+++ website/src/Handler/Project.hs
@@ -8,13 +8,16 @@ import qualified Model.Skeleton as Skeleton
 -- | For MVP, there is one, hard-coded project: Snowdrift
 getSnowdriftProjectR :: Handler Html
 getSnowdriftProjectR = do
-    (donationHistory, nextDonationDate, projectCrowdSize)
+    muid <- fmap entityKey <$> maybeAuth
+    (donationHistory, nextDonationDate, projectCrowdSize, userPledge)
+        :: ([(DonationDay, Int)], DonationDay, Int, Maybe (Entity Pledge))
         <- runDB $ do
             dh <- Skeleton.projectDonationHistory
             [ndd] <- fmap
                 (map (_nextDonationDate . entityVal))
                 (selectList [] [])
             crowd <- count ([] :: [Filter Pledge])
-            return (dh, ndd, crowd)
+            mpledge <- maybe (pure Nothing) (getBy . UniquePledge) muid
+            return (dh, ndd, crowd, mpledge)
     let pledgeAmount :: Double = 0.01 * fromIntegral projectCrowdSize
     $(widget "page/snowdrift-project" "Snowdrift.coop Project")
