1588e8df52c57053d8e0faf92144fd5f1f3b808e
Bryan Richter <b@chreekat.net>
2016-09-23 17:53:10 +0300

    Check payment info before pledging

--- website/src/Handler/Pledge.hs
+++ website/src/Handler/Pledge.hs
@@ -11,22 +11,30 @@ import Handler.Util
 
 postPledgeSnowdriftR :: Handler Html
 postPledgeSnowdriftR = handleDelete' $ do
-    Entity uid _ <- requireAuth
-    dbres <- runDB $ do
-        p <- getBy (UniquePledge uid)
-        maybe (pledge' uid) feignHorror p
-    case dbres of
-        Just t -> alertInfo
-            [shamlet|Your pledge that started on #{show t} is still valid.|]
-        Nothing -> alertSuccess "You are now pledged!"
-    redirect SnowdriftProjectR
+    Entity uid User {..} <- requireAuth
+    case _userStripeCustomer of
+        Nothing -> do
+            alertWarning
+                "Before making a pledge, set up a payment method below"
+            redirect DashboardR
+        _ -> do
+            dbres <- runDB $ do
+                p <- getBy (UniquePledge uid)
+                maybe (pledge' uid) feignHorror p
+            case dbres of
+                Just t -> alertInfo
+                    [shamlet|
+                        Your pledge that started on #{show t} is still valid.
+                    |]
+                Nothing -> alertSuccess "You are now pledged!"
+            redirect SnowdriftProjectR
   where
     pledge' uid = do
         now <- liftIO getCurrentTime
         insert_ (Pledge uid now)
         insert_ (PledgeHistory uid now CreatePledge)
         pure Nothing
-    feignHorror (Entity _ Pledge{..}) = pure (Just _pledgeSince)
+    feignHorror (Entity _ Pledge {..}) = pure (Just _pledgeSince)
     handleDelete' = handleDelete pledgeDeleteFormId deletePledgeSnowdriftR
 
 pledgeDeleteFormId :: Text
