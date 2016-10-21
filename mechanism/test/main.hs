9e911c90e8ec8ad0e539f7de22168671a613d1a4
Bryan Richter <b@chreekat.net>
2016-10-20 17:24:18 -0700

    Split up tests for reuse

--- mechanism/test/main.hs
+++ mechanism/test/main.hs
@@ -56,13 +56,48 @@ dummyStripe = eval <=< viewT
             dummyStripe (k Customer { customerId = cust })
         DeleteCustomerI _ :>>= k -> dummyStripe (k ())
 
+quickCheckDB
+    :: SqlPersistT IO () -> Runner -> (Runner -> PropertyM IO ()) -> IO ()
+quickCheckDB truncation runner prop =
+    quickCheckWith stdArgs{ maxSuccess = 200 } $ monadicIO $ do
+        run (runner truncation)
+        genHistory runner
+        prop runner
+
+genHistory :: Runner -> PropertyM IO ()
+genHistory runner = do
+    targets <- pick (map PPtr <$> listOf (choose (1,1000)))
+    acts <- pick (traverse oneAct targets)
+    let bucket = length acts `div` 5
+        b0 = show (bucket * 5)
+        b1 = show ((bucket + 1) * 5)
+    monitor (label (concat ["N ∈ [", b0,", ",b1 , "]"]))
+    -- Running this concurrently causes "impossible" conflicts during
+    -- upsert! This needs to be fixed! But on the threat matrix, it is both
+    -- unlikely to happen and has minimal side effects (since the DB stays
+    -- consistent, after all.) So I'm punting for now.
+    {- run (void (mapConcurrently (runMech runner) acts))-}
+    run (mapM_ (runMech runner) acts)
+  where
+    oneAct x = frequency
+        [ (10, ActStoreStripeCustomer dummyStripe <$> pure x <*> arbitrary)
+        , (1, pure (ActDeleteStripeCustomer dummyStripe x))
+        , (2, pure (ActStorePledge x))
+        , (1, pure (ActDeletePledge x))
+        ]
+
+type Runner = forall a. SqlPersistT IO a -> IO a
 
 main :: IO ()
 main = runPersistPool $ \runner -> do
     trunq <- runner $ do
         runMigration migrateCrowdmatch
         buildTruncQuery
-    quickCheck (monadicIO (prop_pledgeHist trunq (run . runner)))
+    mapM_
+        (quickCheckDB trunq runner)
+        [ prop_pledgeHist
+        -- , prop_noBadFee
+        ]
   where
     buildTruncQuery = do
         esc <- connEscapeName <$> ask
@@ -72,23 +107,18 @@ main = runPersistPool $ \runner -> do
         -- Don't do it yet
         pure (rawExecute trunq [])
 
-type Runner = forall a. SqlPersistT IO a -> PropertyM IO a
+-- Nobody is ever charged a fee of >10% (max fee = 10%)
+prop_noBadFee :: runner -> PropertyM IO ()
+prop_noBadFee runner = do
+    return ()
 
 -- (PledgeHistory - DeleteHistory) = crowdSize (FetchCrowd)
-prop_pledgeHist :: SqlPersistT IO () -> Runner -> PropertyM IO ()
-prop_pledgeHist truncateTables runner = do
-    targets <- pick (map PPtr <$> listOf (choose (1,1000)))
-    acts <- pick (traverse oneAct targets)
-    let bucket = length acts `div` 5
-        b0 = show (bucket * 5)
-        b1 = show ((bucket + 1) * 5)
-    monitor (label (concat ["N ∈ [", b0,", ",b1 , "]"]))
-    runner truncateTables
-    mapM_ (runMech runner) acts
-    (creat, remov) <- runner $
+prop_pledgeHist :: Runner -> PropertyM IO ()
+prop_pledgeHist runner = do
+    (creat, remov) <- run $ runner $
         partition ((== CreatePledge) . pledgeHistoryAction . entityVal)
             <$> selectList [] []
-    crowd <- fetchCrowdCount runner
+    crowd <- fetchCrowdCount (run . runner)
     monitor (badSize (creat, remov, crowd))
     assert (length creat - length remov == crowdSize crowd)
   where
@@ -102,15 +132,7 @@ prop_pledgeHist truncateTables runner = do
         , show (crowdSize crwd)
         , ")"
         ])
-    oneAct x = frequency
-        [ (10, ActStoreStripeCustomer dummyStripe <$> pure x <*> arbitrary)
-        , (1, pure (ActDeleteStripeCustomer dummyStripe x))
-        , (2, pure (ActStorePledge x))
-        , (1, pure (ActDeletePledge x))
-        ]
-
 
 -- Properties:
---     Nobody is ever charged a fee of >10% (max fee = 10%)
 --     FetchPatron oeualways succeeds
 --     Pledge exists -> StripeCustomer exists
