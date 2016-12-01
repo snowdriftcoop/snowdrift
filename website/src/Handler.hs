97b2a70d572080b1fd35d8da2f3fb1f0376590d8
Bryan Richter <b@chreekat.net>
2016-12-01 14:05:29 -0800

    Fix one title that slipped through

--- website/src/Handler.hs
+++ website/src/Handler.hs
@@ -47,7 +47,7 @@ getDonateR :: Handler Html
 getDonateR = $(widget "page/donate" "Donate")
 
 getAboutR :: Handler Html
-getAboutR = $(widget "page/about" "getAboutR")
+getAboutR = $(widget "page/about" "About")
 
 getSponsorsR :: Handler Html
 getSponsorsR = $(widget "page/sponsors" "Sponsors")
