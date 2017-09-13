0925e626737f4ff4f903014d28ffbf0b3338b13d
Bryan Richter <bryan@snowdrift.coop>
2017-09-13 17:05:48 +0200

    Factor out dashboard header/nav

--- website/src/Handler/Dashboard.hs
+++ website/src/Handler/Dashboard.hs
@@ -14,9 +14,26 @@ import MarkupInstances ()
 
 import Handler.Dashboard.PaymentInfo
 
+data DashboardPage = DashboardPledges | DashboardPaymentInfo
+    deriving (Eq)
+
+dashboardHeader :: DashboardPage -> Patron -> Project -> Widget
+dashboardHeader activePage patron project =
+    let -- list of dashboard pages with routes and titles
+        pages :: [(DashboardPage,Route App,Text)]
+        pages =
+            [ (DashboardPledges,DashboardR,"Pledges")
+            , (DashboardPaymentInfo,PaymentInfoR,"Payment System")
+            ]
+    in $(widgetFile "dashboard-header")
+
 getDashboardR :: Handler Html
 getDashboardR = do
     Entity uid _ <- requireAuth
     (patron, project) <- runDB $ (,) <$> fetchPatron uid <*> fetchProject
     (pledgeNoCSRF, _) <- generateFormPost (renderDivsNoLabels pledgeForm)
     $(widget "page/dashboard" "Dashboard")
+
+getPaymentInfoR :: Handler Html
+getPaymentInfoR = do
+    getPaymentInfo (dashboardHeader DashboardPaymentInfo)
