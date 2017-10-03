c850185828709af9f0be87002ac7510778a98bfe
Bryan Richter <bryan@snowdrift.coop>
2017-10-03 14:28:09 +0200

    Handle missing env variable

--- crowdmatch/app/MakePaymentsMain.hs
+++ crowdmatch/app/MakePaymentsMain.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- | make-payments
 --
@@ -15,16 +16,17 @@ import Data.ByteString.Char8 (pack)
 import Data.Maybe (fromJust)
 import Web.Stripe (StripeKey(..), StripeConfig(..))
 import System.Environment (lookupEnv)
+import Control.Error (runScript, (!?), scriptIO)
 
 import Crowdmatch
 import RunPersist
 
 main :: IO ()
-main = do
-    conf <- fmap
-        (StripeConfig . StripeKey . pack . fromJust)
-        (lookupEnv "STRIPE_SECRET_KEY")
+main = runScript $ do
+    conf <- fmap -- ExceptT String IO
+        (StripeConfig . StripeKey . pack)
+        (lookupEnv "STRIPE_SECRET_KEY" !? "Missing STRIPE_SECRET_KEY in env")
     -- NB! The string passed to runPersistKeter must match the APPNAME used in
     -- keter.sh to deploy the app. Must fix. (Duplicate comment from
     -- CrowdmatchMain.)
-    runPersistKeter "SnowdriftReboot" (makePayments (runStripe conf))
+    scriptIO (runPersistKeter "SnowdriftReboot" (makePayments (runStripe conf)))
