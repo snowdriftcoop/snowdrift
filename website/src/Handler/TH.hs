fc8b7f8e36e672783b81588f97dcc3b5f8dce7f6
Henri Jones <henri.jones@protonmail.com>
2017-10-29 16:30:48 +0000

    Restore accidentally deleted documentation [skip ci]

--- website/src/Handler/TH.hs
+++ website/src/Handler/TH.hs
@@ -6,6 +6,8 @@ import Handler.Util
 
 import Language.Haskell.TH
 
+-- | Generates things of type "Handler Html". Very useful for simple
+-- handlers.
 widget :: String -> Text -> Q Exp
 widget name title =
     [|navbarLayout name (snowdriftTitle title >> $(widgetFile name))|]
