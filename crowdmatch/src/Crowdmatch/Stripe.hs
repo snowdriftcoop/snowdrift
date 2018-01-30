9806c3ac9f8582da9f0410fcac88530e319ad9de
Jason Harrer <jazzyeagle79@gmail.com>
2018-01-30 15:34:44 -0600

    Added dummy class to read StripeRequest types, thanks to fr33domlovers assistance

--- crowdmatch/src/Crowdmatch/Stripe.hs
+++ crowdmatch/src/Crowdmatch/Stripe.hs
@@ -1,17 +1,71 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts  #-}
 
 module Crowdmatch.Stripe where
 
 import Data.Aeson.Types
-import qualified Web.Stripe as S
+import Data.Time.Calendar
+import Data.Time.Clock
+import qualified Data.Text as T
+import Web.Stripe hiding (stripe)
+import qualified Web.Stripe as S (stripe)
+import Web.Stripe.Balance (GetBalanceTransaction)
+import Web.Stripe.Charge (CreateCharge)
+import Web.Stripe.Customer ( Customer (..)
+                           , CreateCustomer
+                           , UpdateCustomer
+                           , DeleteCustomer
+                           , CustomerId (..)
+                           , MetaData (..)
+                           , StripeList (..))
 
-stripe :: FromJSON (S.StripeReturn a)
-       => S.StripeConfig
-       -> S.StripeRequest a
-       -> IO (Either S.StripeError (S.StripeReturn a))
+-- newtype StripeState = StripeState { lastCharge :: Maybe Cents } deriving (Eq, Show)
+
+--class StripeDummyConst a where
+--    stripeDummyConst :: StripeReturn a
+--
+--class StripeDummyPure a where
+--    stripeDummyPure :: StripeRequest a -> StripeReturn a
+
+class StripeDummyIO a where
+    stripeDummyIO :: StripeRequest a -> IO (StripeReturn a)
+
+stripe :: (FromJSON (StripeReturn a), StripeDummyIO a)
+       => StripeConfig
+       -> StripeRequest a
+       -> IO (Either StripeError (StripeReturn a))
 stripe c s 
-    | (S.secretKey c) == 
-            S.StripeKey "test" = error "Snowdrift Stripe Test"
-    | otherwise               = S.stripe c s
-    
+    | (secretKey c) == StripeKey "test" = Right <$> stripeDummyIO s
+    | otherwise                         = S.stripe c s
+
+instance StripeDummyIO CreateCustomer where
+    stripeDummyIO _ = return $ dummyCustomer
+
+instance StripeDummyIO UpdateCustomer where
+    stripeDummyIO _ = return $ dummyCustomer
+
+instance StripeDummyIO DeleteCustomer where
+    stripeDummyIO _ = undefined
+
+instance StripeDummyIO CreateCharge where
+    stripeDummyIO _ = undefined
+
+instance StripeDummyIO GetBalanceTransaction where
+    stripeDummyIO _ = undefined
+
+
+dummyCustomer :: Customer
+dummyCustomer = Customer T.empty
+                         (UTCTime (fromGregorian 2018 1 1) (secondsToDiffTime 0))
+                         (CustomerId "dummy")
+                         False
+                         Nothing
+                         Nothing
+                         False
+                         (StripeList [] T.empty T.empty Nothing False)
+                         Nothing
+                         0
+                         (StripeList [] T.empty T.empty Nothing False)
+                         Nothing
+                         Nothing
+                         (MetaData [])
