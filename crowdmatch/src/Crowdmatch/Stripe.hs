3776d2225b3023ba50e26368af4887a5fc9cbcb4
fr33domlover <fr33domlover@riseup.net>
2018-01-30 23:55:24 +0200

    Crowdmatch: Add some comments and reorder things in dummy Stripe code

--- crowdmatch/src/Crowdmatch/Stripe.hs
+++ crowdmatch/src/Crowdmatch/Stripe.hs
@@ -1,14 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts  #-}
 
-module Crowdmatch.Stripe where
+module Crowdmatch.Stripe
+    ( StripeDummy (..)
+    , stripe
+    )
+where
 
 import Data.Aeson.Types
 import Data.Time.Calendar
 import Data.Time.Clock
-import qualified Data.Text as T
 import Web.Stripe hiding (stripe)
-import qualified Web.Stripe as S (stripe)
 import Web.Stripe.Balance (GetBalanceTransaction)
 import Web.Stripe.Charge (CreateCharge)
 import Web.Stripe.Customer ( Customer (..)
@@ -19,53 +21,63 @@ import Web.Stripe.Customer ( Customer (..)
                            , MetaData (..)
                            , StripeList (..))
 
+import qualified Data.Text as T
+import qualified Web.Stripe as S (stripe)
+
 -- newtype StripeState = StripeState { lastCharge :: Maybe Cents } deriving (Eq, Show)
 
---class StripeDummyConst a where
---    stripeDummyConst :: StripeReturn a
+-- | A typeclass for Stripe requests (such as 'CreateCustomer') which can be
+-- answered with dummy data without any HTTP requests to Stripe itself. We're
+-- allowing the dummy data generator access the request and do IO actions, but
+-- these things can be removed to simplify things if they aren't needed (for
+-- example if the dummy data is just hardcoded).
+class StripeDummy a where
+    -- | Provide a response to the Stripe request with some dummy data, without
+    -- talking to Stripe itself.
+    stripeDummy :: StripeRequest a -> IO (StripeReturn a)
+
+-- | Fill a Stripe request, either by talking to the real Stripe or by
+-- providing some dummy data for development or testing.
 --
---class StripeDummyPure a where
---    stripeDummyPure :: StripeRequest a -> StripeReturn a
-
-class StripeDummyIO a where
-    stripeDummyIO :: StripeRequest a -> IO (StripeReturn a)
-
-stripe :: (FromJSON (StripeReturn a), StripeDummyIO a)
+-- Currently the decision is made using the Stripe key: If the key is `test`
+-- then dummy data is provided. Otherwise, the real Stripe API is contacted by
+-- HTTP request and its response returned.
+stripe :: (FromJSON (StripeReturn a), StripeDummy a)
        => StripeConfig
        -> StripeRequest a
        -> IO (Either StripeError (StripeReturn a))
-stripe c s 
-    | (secretKey c) == StripeKey "test" = Right <$> stripeDummyIO s
-    | otherwise                         = S.stripe c s
-
-instance StripeDummyIO CreateCustomer where
-    stripeDummyIO _ = return $ dummyCustomer
-
-instance StripeDummyIO UpdateCustomer where
-    stripeDummyIO _ = return $ dummyCustomer
-
-instance StripeDummyIO DeleteCustomer where
-    stripeDummyIO _ = undefined
-
-instance StripeDummyIO CreateCharge where
-    stripeDummyIO _ = undefined
-
-instance StripeDummyIO GetBalanceTransaction where
-    stripeDummyIO _ = undefined
-
+stripe c s
+    | secretKey c == StripeKey "test" = Right <$> stripeDummy s
+    | otherwise                       = S.stripe c s
 
 dummyCustomer :: Customer
-dummyCustomer = Customer T.empty
-                         (UTCTime (fromGregorian 2018 1 1) (secondsToDiffTime 0))
-                         (CustomerId "dummy")
-                         False
-                         Nothing
-                         Nothing
-                         False
-                         (StripeList [] T.empty T.empty Nothing False)
-                         Nothing
-                         0
-                         (StripeList [] T.empty T.empty Nothing False)
-                         Nothing
-                         Nothing
-                         (MetaData [])
+dummyCustomer = Customer
+    T.empty
+    (UTCTime (fromGregorian 2018 1 1) (secondsToDiffTime 0))
+    (CustomerId "dummy")
+    False
+    Nothing
+    Nothing
+    False
+    (StripeList [] T.empty T.empty Nothing False)
+    Nothing
+    0
+    (StripeList [] T.empty T.empty Nothing False)
+    Nothing
+    Nothing
+    (MetaData [])
+
+instance StripeDummy CreateCustomer where
+    stripeDummy _ = return $ dummyCustomer
+
+instance StripeDummy UpdateCustomer where
+    stripeDummy _ = return $ dummyCustomer
+
+instance StripeDummy DeleteCustomer where
+    stripeDummy _ = undefined
+
+instance StripeDummy CreateCharge where
+    stripeDummy _ = undefined
+
+instance StripeDummy GetBalanceTransaction where
+    stripeDummy _ = undefined
