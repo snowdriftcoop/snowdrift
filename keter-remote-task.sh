f941954ff298d09db11ae360e4097aba56ce5c62
Bryan Richter <bryan@snowdrift.coop>
2018-03-31 19:18:00 -0400

    Init keter-remote-task.sh

---
+++ keter-remote-task.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+#
+# keter-remote-task.sh: executed on a remote build server by keter-remote.sh.
+#
+
+set -e
+
+log () {
+    echo -n ":::: (worker) "
+    echo $@
+}
+
+main () {
+    if [ $# -ne 2 ]
+    then
+        log "Incorrect worker arguments: [$*]"
+        log "Expected: [BUILDREV APPNAME]"
+        exit 1
+    fi
+    BUILDREV=$1
+    export APPNAME=$2
+
+    mkdir -p keter-remote-task
+    cd keter-remote-task
+    if [ ! -d snowdrift ]
+    then
+        log "Cloning repo"
+        git clone https://git.snowdrift.coop/sd/snowdrift.git
+        cd snowdrift
+    else
+        cd snowdrift
+        log "Fetching repo"
+        git fetch
+    fi
+    log "Switching to build rev ${BUILDREV}"
+    git reset --hard ${BUILDREV}
+    log "Building deployment bundle"
+    DEPLOY=false ./keter.sh
+    mv website/${APPNAME}.keter ../..
+}
+main $@
