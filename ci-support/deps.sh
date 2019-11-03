791d4e6e1f3969dbc1913aaf2e85f34027c04bda
Bryan Richter <bryan@snowdrift.coop>
2019-11-03 13:08:10 +0200

    deps.sh: Use correct stack and apt-get

--- ci-support/deps.sh
+++ ci-support/deps.sh
@@ -2,13 +2,18 @@
 
 set -Eeuo pipefail
 
+# Project-specific override
+stack () {
+    command stack ${SD_STACK_ARGS?-} --workdir .stack-deploy "$@"
+}
+
 ## System setup
 
-apt --quiet update
-apt --quiet --assume-yes install curl postgresql libgmp-dev libpq-dev
+apt-get --quiet update
+apt-get --quiet --assume-yes install curl postgresql libgmp-dev libpq-dev
 curl -sSL https://get.haskellstack.org/ | sh
 
 
 ## Dependencies installation
 
-$STACK test --no-run-tests --only-dependencies
+stack test --no-run-tests --only-dependencies
