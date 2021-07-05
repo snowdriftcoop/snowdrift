a7f8a3fe39a1023026877462e97dba920d3910b3
Stephen Michel <git@smichel.me>
2021-07-05 11:42:10 -0400

    Refactor CrowdmatchMain to be beginner-friendlier
    - Use parentheses instead of $ for grouping. This is standard in other
      languages, so it's more readable to a haskell-newbie.
    
    - Handle each argument case independently. Potentially more places for
      bugs, but easier to follow the execution flow.
    
    - Refactor out the `runCrowdmatch` function, so the comment will be
      closer to the line it describes

--- crowdmatch/app/CrowdmatchMain.hs
+++ crowdmatch/app/CrowdmatchMain.hs
@@ -13,22 +13,32 @@ usage :: IO a -- exits
 usage = do
   putStrLn "usage: crowdmatch [YEAR MONTH DAY]"
   putStrLn "e.g. crowdmatch 2019 09 01"
-  exitWith $ ExitFailure 2
+  exitWith (ExitFailure 2)
 
+-- In the future we should use ISO 8601 format dates. Newer versions of the
+-- `time` library are able to parse them, so we can replace parseDate with
+-- import Data.Time.Format.ISO8601 (iso8601ParseM)
+-- However, the `time` library is a common dependency so we can't just bump
+-- its version; we need to update all of our deps.. which we ought to do anyway,
+-- but will take more time (heh) than I want to spend right now.
 parseDate :: String -> String -> String -> Maybe Day
-parseDate y m d = do
-  year <- readMaybe y
-  month <- readMaybe m
-  day <- readMaybe d
-  return $ fromGregorian year month day
+parseDate yyyy mm dd = do
+  year <- readMaybe yyyy
+  month <- readMaybe mm
+  day <- readMaybe dd
+  return (fromGregorian year month day)
 
 -- NB! The string passed to runPersistKeter must match the APPNAME used in
 -- keter.sh to deploy the app. Must fix.
+runCrowdmatch :: Day -> IO ()
+runCrowdmatch day = runPersistKeter "SnowdriftReboot" (crowdmatch day)
+
 main :: IO ()
 main = do
   args <- getArgs
-  day <- case args of
-    [] -> utctDay <$> getCurrentTime
-    y : m : d : [] -> maybe usage return $ parseDate y m d
+  case args of
+    [] -> do
+      day <- utctDay <$> getCurrentTime
+      runCrowdmatch day
+    yyyy : mm : dd : [] -> maybe usage runCrowdmatch (parseDate yyyy mm dd)
     _ -> usage
-  runPersistKeter "SnowdriftReboot" $ crowdmatch day
