29978826fbda7b3d3aa97c4159d99ad958058298
Stephen Michel <git@smichel.me>
2021-07-05 15:12:22 -0400

    Require stricter crowdmatch input, for #627
    - Before, if you passed no arguments, the crowdmatch program would run
      for today's date. Now it requires arguments and prints usage info.
      Harder to accidentally run it.
    
    - Before, the crowdmatch program would allow you to pass any date, even
      one in the future!  Wouldn't want to typo 2021 -> 2022! Now, it will
      only accept today's date or a date in the past.
    
    This will make me a little more confident in running the crowmatches.

--- crowdmatch/app/CrowdmatchMain.hs
+++ crowdmatch/app/CrowdmatchMain.hs
@@ -11,22 +11,34 @@ import Text.Read (readMaybe)
 
 usage :: IO a -- exits
 usage = do
-  putStrLn "usage: crowdmatch [YEAR MONTH DAY]"
+  putStrLn "usage: crowdmatch YEAR MONTH DAY"
   putStrLn "e.g. crowdmatch 2019 09 01"
   exitWith (ExitFailure 2)
 
 -- In the future we should use ISO 8601 format dates. Newer versions of the
--- `time` library are able to parse them, so we can replace parseDate with
+-- `time` library are able to parse them, so we can replace parseDay with
 -- import Data.Time.Format.ISO8601 (iso8601ParseM)
 -- However, the `time` library is a common dependency so we can't just bump
 -- its version; we need to update all of our deps.. which we ought to do anyway,
 -- but will take more time (heh) than I want to spend right now.
-parseDate :: String -> String -> String -> Maybe Day
-parseDate yyyy mm dd = do
+parseDay :: String -> String -> String -> Maybe Day
+parseDay yyyy mm dd = do
   year <- readMaybe yyyy
   month <- readMaybe mm
   day <- readMaybe dd
   return (fromGregorian year month day)
+
+parseDate :: String -> String -> String -> IO Day
+parseDate yyyy mm dd = do
+  date <- maybe usage return (parseDay yyyy mm dd)
+  today <- utctDay <$> getCurrentTime
+  ensureBefore today date
+  where
+    ensureBefore today date
+      | date <= today = return date
+      | otherwise = do
+        putStrLn ("ERR: DATE must not be in the future. Provided date was: " ++ show date)
+        exitWith (ExitFailure 3)
 
 -- NB! The string passed to runPersistKeter must match the APPNAME used in
 -- keter.sh to deploy the app. Must fix.
@@ -37,8 +49,7 @@ main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [] -> do
-      day <- utctDay <$> getCurrentTime
+    yyyy : mm : dd : [] -> do
+      day <- parseDate yyyy mm dd
       runCrowdmatch day
-    yyyy : mm : dd : [] -> maybe usage runCrowdmatch (parseDate yyyy mm dd)
     _ -> usage
